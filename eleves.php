ddcf0caa9df0e37a11b42d125ec446abd2511cab
liloo2040 <e.dufour.89@gmail.com>
2016-04-29 10:33:07 +0200

    Initial commit

---
+++ eleves.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dufour
+ * Date: 29/04/16
+ * Time: 10:17
+ */
+
+$students = [
+    "Emmanuel" => 42,
+    "Thierry" => 51,
+    "Pascal" => 45,
+    "Eric" => 48,
+    "Nicolas" => 19
+];
